af9bf8b675ff2c87809fea527fd4d604cbacfa66
Guilherme Smith <guilhermessmith2014@gmail.com>
2025-12-08 09:04:22 -0300

    feat: bill finished

--- src/pages/bill/BillForm.jsx
+++ src/pages/bill/BillForm.jsx
@@ -175,14 +175,37 @@ const BillForm = () => {
                 toast.warning(resData.message);
             }
         } catch (error) {
-            toast.error('Erro ao criar categoria, contate o suporte!');
+            toast.error('Erro ao criar conta, contate o suporte!');
+            console.error(error);
         } finally {
             setIsLoading(false);
         }
     }
 
     const update = async (data) => {
+        try {
+            setIsLoading(true);
 
+            const endpoint = `bill/${params.id}`;
+            const completeUrl = `${apiUrl}${endpoint}`;
+
+            const res = await fetch(completeUrl, apiOptions('PUT', data));
+            console.table(res);
+            const resData = await res.json();
+            console.table(resData);
+
+            if (res.ok) {
+                toast.success('Conta atualizada!!');
+                navigate(`/bill/form/${params.id}`);
+            } else {
+                toast.warning(resData.message);
+            }
+        } catch (error) {
+            toast.error('Erro ao atualizar conta, contate o suporte!');
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     const onSubmit = async (data) => {
@@ -221,78 +244,94 @@ const BillForm = () => {
                 <h1 className='fw-bold'>Contas recorrentes</h1>
                 <p>Gerencia sua contas recorrentes</p>
             </div>
-            {/* Formulário */}
-            <form action="#" className='card shadow-sm p-3' onSubmit={handleSubmit(onSubmit)} >
-                {/* Botões */}
-                <div className='form-line'>
-                    <button type='button' onClick={handleNewButton} className='btn btn-primary' >Novo</button>
-                    <button type='submit' disabled={isWaitingResponse || isLoading} className='btn btn-success'>Salvar</button>
-                    <NavLink to='/bill/list' className={'btn btn-dark'} >Listar</NavLink>
+            <section className='form-line'>
+                {/* Formulário */}
+                <form action="#" className='card shadow-sm p-3' onSubmit={handleSubmit(onSubmit)} >
+                    {/* Título */}
+                    <div className='text-center'>
+                        <h2>Formulário</h2>
+                        <p>Altere sua conta a pagar aqui</p>
+                    </div>
+                    {/* Botões */}
+                    <div className='form-line'>
+                        <button type='button' onClick={handleNewButton} className='btn btn-primary' >Novo</button>
+                        <button type='submit' disabled={isWaitingResponse || isLoading} className='btn btn-success'>Salvar</button>
+                        <NavLink to='/bill/list' className={'btn btn-dark'} >Listar</NavLink>
+                    </div>
+                    {/* Campos */}
+                    <div>
+                        {/* Descrição e Categoria */}
+                        <div className='form-line'>
+                            {/* Descrição */}
+                            <div className='mb-3'>
+                                <label htmlFor="descricao" className='form-label'>Descrição</label>
+                                <input type="text" className='form-control ' id='descricao' {...register('descricao')} placeholder='Conta de Energia' />
+                            </div>
+                            {/* Categoria */}
+                            <div className='mb-3'>
+                                <label htmlFor="id-categoria" className='form-label'>Categoria</label>
+                                <select id="id-categoria" className='form-select' {...register('id_categoria')} disabled={categories.length == 0}>
+                                    <option value="" >{categories.length === 0 ? 'Nenhuma categoria cadastrada' : 'Escolha uma categoria'}</option>
+                                    {categories.length > 0 && categories.map((cat) => (
+                                        <option key={cat.id} value={cat.id}>{cat.descricao_visual}</option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                        {/* Valor base e Ativo */}
+                        <div className='form-line'>
+                            {/* valor_base */}
+                            <div className='mb-3'>
+                                <label htmlFor="valor_base" className='form-label'>Valor base</label>
+                                <input type="tel" step='0.01' min='0' className='form-control' id='valor_base' {...register('valor_base')} placeholder='R$ 0,00' />
+                            </div>
+                            {/* Ativo */}
+                            <div className='mb-3'>
+                                <label htmlFor="ativo" className='form-label'>Ativo</label>
+                                <select id="ativo" className='form-select' {...register('ativo')}>
+                                    <option value="S">Sim</option>
+                                    <option value="N">Não</option>
+                                </select>
+                            </div>
+                        </div>
+                        {/* Recorrência, Dia fixo e Mês de início */}
+                        <div className='form-line'>
+                            {/* Recorrência */}
+                            <div className='mb-3'>
+                                <label htmlFor="recorrencia" className='form-label'>Recorrência</label>
+                                <select id='recorrencia' className='form-select small' {...register('tipo_recorrencia')} >
+                                    <option value="M">Mensal</option>
+                                    <option value="A">Anual</option>
+                                </select>
+                            </div>
+                            {/* Dia fixo */}
+                            <div className='mb-3'>
+                                <label htmlFor="dia-fixo" className='form-label'>Dia fixo</label>
+                                <input type="tel" className='form-control small' id='dia-fixo' {...register('dia_fixo')} />
+                            </div>
+                            {/* Mês de início */}
+                            <div className='mb-3'>
+                                <label htmlFor="data-inicio" className='form-label'>
+                                    Data de Início
+                                </label>
+                                <input type="date" className='form-control' id='data-inicio' {...register('mes_inicial')} />
+                                <small className='ms-2 text-muted'>Usaremos apenas mês e ano</small>
+                            </div>
+                        </div>
+                        {/* Observações */}
+                        <div className='mb-3'>
+                            <label htmlFor="observacoes" className='form-label'>Observações</label>
+                            <textarea id='observacoes' className='form-control' placeholder='Ex: pagar só depois do dia 10' {...register('observacoes')}></textarea>
+                        </div>
+                    </div>
+                </form>
+                {/* Pagamentos */}
+                <div className='card shadow-sm p-3 table-container'>
+                    <table>
+                        
+                    </table>
                 </div>
-                {/* Descrição e Categoria */}
-                <div className='form-line'>
-                    {/* Descrição */}
-                    <div className='mb-3'>
-                        <label htmlFor="descricao" className='form-label'>Descrição</label>
-                        <input type="text" className='form-control ' id='descricao' {...register('descricao')} placeholder='Conta de Energia' />
-                    </div>
-                    {/* Categoria */}
-                    <div className='mb-3'>
-                        <label htmlFor="id-categoria" className='form-label'>Categoria</label>
-                        <select id="id-categoria" className='form-select' {...register('id_categoria')} disabled={categories.length == 0}>
-                            <option value="" >{categories.length === 0 ? 'Nenhuma categoria cadastrada' : 'Escolha uma categoria'}</option>
-                            {categories.length > 0 && categories.map((cat) => (
-                                <option key={cat.id} value={cat.id}>{cat.descricao_visual}</option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
-                {/* Valor base e Ativo */}
-                <div className='form-line'>
-                    {/* valor_base */}
-                    <div className='mb-3'>
-                        <label htmlFor="valor_base" className='form-label'>Valor base</label>
-                        <input type="tel" step='0.01' min='0' className='form-control' id='valor_base' {...register('valor_base')} placeholder='R$ 0,00' />
-                    </div>
-                    {/* Ativo */}
-                    <div className='mb-3'>
-                        <label htmlFor="ativo" className='form-label'>Ativo</label>
-                        <select id="ativo" className='form-select' {...register('ativo')}>
-                            <option value="S">Sim</option>
-                            <option value="N">Não</option>
-                        </select>
-                    </div>
-                </div>
-                {/* Recorrência, Dia fixo e Mês de início */}
-                <div className='form-line'>
-                    {/* Recorrência */}
-                    <div className='mb-3'>
-                        <label htmlFor="recorrencia" className='form-label'>Recorrência</label>
-                        <select id='recorrencia' className='form-select small' {...register('tipo_recorrencia')} >
-                            <option value="M">Mensal</option>
-                            <option value="A">Anual</option>
-                        </select>
-                    </div>
-                    {/* Dia fixo */}
-                    <div className='mb-3'>
-                        <label htmlFor="dia-fixo" className='form-label'>Dia fixo</label>
-                        <input type="tel" className='form-control small' id='dia-fixo' {...register('dia_fixo')} />
-                    </div>
-                    {/* Mês de início */}
-                    <div className='mb-3'>
-                        <label htmlFor="data-inicio" className='form-label'>
-                            Data de Início
-                        </label>
-                        <input type="date" className='form-control' id='data-inicio' {...register('mes_inicial')} />
-                        <small className='ms-2 text-muted'>Usaremos apenas mês e ano</small>
-                    </div>
-                </div>
-                {/* Observações */}
-                <div className='mb-3'>
-                    <label htmlFor="observacoes" className='form-label'>Observações</label>
-                    <textarea id='observacoes' className='form-control' placeholder='Ex: pagar só depois do dia 10' {...register('observacoes')}></textarea>
-                </div>
-            </form>
+            </section>
             <ToastContainer position='bottom-right' />
             {isLoading ? <Loading /> : <></>}
         </article>
